204acb77879822b5f742754ad6ede159819016b3
chrisdwoodby <chrisdwoodby@gmail.com>
2021-07-03 17:21:11 -0500

    assignment complete

--- node-req-count/server.js
+++ node-req-count/server.js
@@ -10,8 +10,32 @@ var server = http.createServer(function(request, response) {
 
   if (request.method === 'POST') {
     // YOUR CODE HERE
+    if (request.url.includes('/dogs')) {
+      globalCounter.dogs = 0;
+      request.on('data', function() {
+        globalCounter.dogs += 1;
+      });
+      response.writeHead(201, {'Content-Type': 'application/json'});
+      response.end(JSON.stringify(globalCounter.dogs));
+    }
+    if (request.url.includes('/cats')) {
+      globalCounter.cats = 0;
+      request.on('data', function() {
+        globalCounter.cats += 1;
+      });
+      response.writeHead(201, {'Content-Type': 'application/json'});
+      response.end(JSON.stringify(globalCounter.cats));
+    }
   } else if (request.method === 'GET') {
     // YOUR CODE HERE
+    if (globalCounter.dogs) {
+      response.writeHead(200, {'Content-Type': 'application/json'});
+      response.end(JSON.stringify(globalCounter.dogs));
+    }
+    if (globalCounter.cats) {
+      response.writeHead(200, {'Content-Type': 'application/json'});
+      response.end(JSON.stringify(globalCounter.cats));
+    }
   } else {
     response.statusCode = 404;
     response.end();
@@ -20,3 +44,12 @@ var server = http.createServer(function(request, response) {
 
 // Do not edit this line
 module.exports = server;
+
+
+// * A **POST** to `/dogs` should set `globalCounter.dogs = 1`.
+// * A subsequent **GET** to `/dogs` should return `1`.
+// * A subsequent **POST** to `/dogs` should set `globalCounter.dogs = 2`.
+// * A subsequent **GET** to `/dogs` should return `2`.
+// * A subsequent **GET** to `/cats` should return an empty response, as it has not yet been set.
+// * A subsequent **POST** to `/cats` should set `globalCounter.cats = 1`.
+// * A subsequent **GET** to `/cats` should return `1`.
